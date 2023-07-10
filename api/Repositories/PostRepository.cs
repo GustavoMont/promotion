af60fd783ffaade864c810cd0c97bd6515bb4af5
GustavoMont <lgmluisgm@gmail.com>
2023-07-10 13:51:27 -0300

    fix: fix conflicts in PostRepository

--- api/Repositories/PostRepository.cs
+++ api/Repositories/PostRepository.cs
@@ -25,22 +25,33 @@ public class PostRepository
     public async Task<Post?> GetByIdAsync(int id)
     {
         return await _context.Posts
-            .Include(p => p.User)
-            .Include(p => p.Address.City)
             .Include(p => p.Complaints)
+            .Include(p => p.User)
+            .Include(p => p.Address)
             .AsNoTracking()
             .FirstOrDefaultAsync(post => post.Id == id);
+    }
+
+    public async Task<List<Post>> ListComplaintedPostsAsync(int min = 5)
+    {
+        return await _context.Posts
+            .AsNoTracking()
+            .Where(p => p.Complaints.Count >= min)
+            .OrderByDescending(p => p.Complaints.Count)
+            .Include(p => p.Complaints)
+            .ToListAsync();
     }
 
     public async Task<List<Post>> GetAllAsync(int? userId = null)
     {
         return await _context.Posts
+            .OrderByDescending(post => post.Id)
             .AsNoTracking()
-            .Where(p => userId == null || p.UserId == userId)
+            .Include(p => p.Complaints)
             .OrderByDescending(post => post.Id)
             .Include(p => p.User)
-            .Include(p => p.Address.City)
-            .Include(p => p.Complaints)
+            .Include(p => p.Address)
+            .Where(p => userId == null || p.UserId == userId)
             .ToListAsync();
     }
 }
