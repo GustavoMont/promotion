bb4f7c5f38c5021d46c2f497dd3aef51b7eae6a1
pedro-santos28 <phdss04@gmail.com>
2023-07-10 09:29:52 -0500

    restructure: some changes on full post card

--- frontend/components/posts/FullPostCard.tsx
+++ frontend/components/posts/FullPostCard.tsx
@@ -8,6 +8,7 @@ import { useForm } from "react-hook-form";
 import { Button } from "../common/Button";
 import ReportPostModal from "./ReportPostModal";
 import { FlagIcon } from "@heroicons/react/24/outline";
+import { Title } from "../Typograph/Title";
 
 type FullPostCardProps = {
   post: Post;
@@ -17,42 +18,55 @@ const FullPostCard = ({ post }: FullPostCardProps) => {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className="h-full p-4 max-w-[635px]">
+    <div className="flex flex-col border-2 border-black h-full p-4 gap-8 md:flex-row w-full">
       {showModal ? <ReportPostModal setShowModal={setShowModal} /> : null}
 
-      <header className="flex justify-between m-2 items-start sm:flex-row sm:border-black">
-        <div className="flex gap-2">
-          <img
-            src={post.user.avatar || "bonzi.jpg"}
-            className="rounded-full w-[64px] h-[64px]"
-          />
-          <div>{post?.user ? post?.user?.name : "Nome de usuário"}</div>
-        </div>
-        <div className="flex flex-col items-end gap-2">
-          <h2 className="card-title">{post.title}</h2>
-          <FlagIcon
-            className="h-10 w-10 text-danger font-medium p-1 rounded-md  cursor-pointer hover:bg-dark-danger hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105"
-            onClick={() => setShowModal(true)}
-          />
-        </div>
-      </header>
+      <div className="flex flex-col gap-4 bg-white  p-4 shadow rounded-t-lg border-t-8 border-t-primary w-full">
+        <div className="flex gap-2 items-center justify-between">
+          <div className="flex flex-col items-center gap-1">
+            <img
+              src={post.user.avatar || "bonzi.jpg"}
+              className="rounded-full w-[40px] h-[40px]"
+            />
+            <div>{post?.user ? post?.user?.name : "Nome de usuário"}</div>
+          </div>
 
-      <main>
-        <figure>
-          <img src={"https://placehold.co/600X400"} alt="Album" />
-        </figure>
-        <div className="my-2 flex flex-col gap-0.5 items-end">
-          <p className="text-red-500 font-thin text-xs line-through">
-            {currencyFormatter(post.oldPrice)}
-          </p>
-          <p className="text-accent font-semibold text-xl">
-            {currencyFormatter(post.promotionPrice)}
-          </p>
+          <div className="flex flex-col items-end gap-2">
+            <FlagIcon
+              className="h-10 w-10 text-danger font-medium p-1 rounded-md  cursor-pointer hover:bg-dark-danger hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105"
+              onClick={() => setShowModal(true)}
+            />
+          </div>
         </div>
-        <p>{post.description}</p>
+        <figure>
+          <img
+            className="max-w-[300px] max-h-[300px] rounded"
+            src={"https://placehold.co/600X400"}
+            alt="Album"
+          />
+        </figure>
+
+        <div className="my-2 flex flex-col gap-2 justify-between items-start">
+          <Title>{post.title}</Title>
+          <div className="flex flex-col gap-1">
+            <p className="text-red-500 font-thin text-xs line-through">
+              {currencyFormatter(post.oldPrice)}
+            </p>
+            <p className="text-accent font-semibold text-xl">
+              {currencyFormatter(post.promotionPrice)}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4 bg-white  p-4 shadow rounded-t-lg border-t-8 border-t-primary w-full">
+        <div>
+          <Title>Mais informações</Title>
+          <p>{post.description}</p>
+        </div>
 
         <AddressCard address={post.address} />
-      </main>
+      </div>
     </div>
   );
 };
--- frontend/components/posts/address/AddressCard.tsx
+++ frontend/components/posts/address/AddressCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Address } from "@/models/Address";
+import { Title } from "@/components/Typograph/Title";
 
 interface AddressCardProps {
   address: Address;
@@ -11,8 +12,8 @@ const AddressCard: React.FC<AddressCardProps> = ({ address }) => {
       {!address ? (
         <p className="text-red-400">* Não foi cadastrado o endereço</p>
       ) : (
-        <div className="bg-white rounded-lg p-4 shadow">
-          <h2 className="text-2xl font-bold mb-4">Endereço</h2>
+        <div>
+          <Title>Endereço</Title>
           <p className="text-gray-600">{address?.street}</p>
           <p className="text-gray-600">
             {address?.city?.name || "Cidade não informada"}
