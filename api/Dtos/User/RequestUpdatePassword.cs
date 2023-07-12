3d137df01a374921161377bae100b52c4fc0f589
GustavoMont <lgmluisgm@gmail.com>
2023-07-12 20:02:28 -0300

    save changes

--- api/Dtos/User/RequestUpdatePassword.cs
+++ api/Dtos/User/RequestUpdatePassword.cs
@@ -4,12 +4,15 @@ namespace api.Dtos.User;
 
 public class RequestUpdatePassword
 {
+    public string CurrentPassword { get; set; } = null!;
+
     [MinLength(8, ErrorMessage = "A senha deve ter o mínimo {1} caracteres")]
     [RegularExpression(
         "^(?=.*[A-Za-z])(?=.*?[0-9]).{8,}$",
         ErrorMessage = "A senha deve ter no mínimo uma letra e um número"
     )]
-    [Required(ErrorMessage = "Senha é um campo obrigatório")]
-    public string? CurrentPassword { get; set; }
-    public string? NewPassword { get; set; }
+    public string NewPassword { get; set; } = null!;
+
+    [Required]
+    public string ConfirmPassword { get; set; } = null!;
 }
